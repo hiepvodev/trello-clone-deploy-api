8cf91b2b517d9e5c031151dfc93c3ae476a4ac66
hiep <vanhiep2102@gmail.com>
2021-08-28 23:45:03 +0700

    try again

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { connectDB } from '@/config/mongodb'
-import { env } from '@/config/environment'
+// import { env } from '@/config/environment'
 import { apiV1 } from '@/routes/v1'
 import cors from 'cors'
 import { corsOptions } from './config/cors'
 
 
-const port = env.APP_PORT
-const hostName = env.APP_HOST
+// const port = env.APP_PORT
+// const hostName = env.APP_HOST
 
 //connect DB
 connectDB()
@@ -34,7 +34,7 @@ const bootServer = () => {
   //   console.log(`Trello clone app listening at http://${hostName}:${port}`)
   // })
   //Support heorku deploy
-  app.listen(process.env.PORT, () => {
-    console.log(`Trello clone app listening at ${process.env.PORT}`)
+  app.listen(process.env.APP_PORT, () => {
+    console.log(`Trello clone app listening at ${process.env.APP_PORT}`)
   })
 }
